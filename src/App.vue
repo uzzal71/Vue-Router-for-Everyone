fb55c5b7b2350a2a0fa3fe052c39219fe584be1f
uzzal71 <uzzalroy.acm@gmail.com>
2022-11-25 00:49:39 +0600

    Create Vue Routes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/hawaii">hawaii</router-link>
+    <router-link to="/brazil">Brazil</router-link> |
+    <router-link to="/jamaica">Jamaica</router-link> |
+    <router-link to="/hawaii">Hawaii</router-link> |
+    <router-link to="/panama">Panama</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import BrazilView from "../views/BrazilView.vue";
 import HawaiiView from "../views/HawaiiView.vue";
+import JamaicaView from "../views/JamaicaView.vue";
+import PanamaView from "../views/PanamaView.vue";
 
 const routes = [
   {
@@ -27,6 +29,16 @@ const routes = [
     path: "/hawaii",
     name: "hawaii",
     component: HawaiiView,
+  },
+  {
+    path: "/jamaica",
+    name: "jamaica",
+    component: JamaicaView,
+  },
+  {
+    path: "/panama",
+    name: "panama",
+    component: PanamaView,
   },
 ];
 
