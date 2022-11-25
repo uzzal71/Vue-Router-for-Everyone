d4a79a18a4dbe32e19079fbef9d0900ad1435b29
uzzal71 <uzzalroy.acm@gmail.com>
2022-11-26 00:45:57 +0600

    Create Routes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,9 +4,15 @@
 
 <script>
 // @ is an alias to /src
+import store from '@/store';
 
 export default {
   name: "HomeView",
   components: {},
+  data() {
+    return {
+      destinations: store.destinations
+    }
+  }
 };
 </script>
