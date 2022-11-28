1fcfd55ef990bc3a53a13c2450c582c2137af5dc
uzzal71 <uzzalroy.acm@gmail.com>
2022-11-29 01:09:04 +0600

    Vue Router Dynamic Routes

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,3 +1,25 @@
 <template>
-    <p>The destination: {{ this.$route.params.id }}</p>
+    <section class="destination">
+        <h2>{{ destination.name }}</h2>
+        <div class="destination-details">
+            <img :src="require(`@/assets/${destination.imgage}`)" alt="destination.name" />
+        </div>
+    </section>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            destinationId: this.$route.params.id
+        }
+    },
+    computed: {
+        destination() {
+            return store.destinations.find(
+                destination => destination.id === this.destinationId
+            )
+        }
+    }
+}
+</script>
